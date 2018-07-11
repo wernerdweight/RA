3c90d55824cf242f1b4f646d996d475277eace8b
Werner Dweight <wd@wernerdweight.com>
2018-07-11 02:05:08 +0200

    More tests

--- Tests/ArrayMethodsTest.php
+++ Tests/ArrayMethodsTest.php
@@ -4,27 +4,76 @@ declare(strict_types=1);
 namespace WernerDweight\RA\Tests;
 
 use PHPUnit\Framework\TestCase;
+use WernerDweight\RA\Exception\RAException;
+use WernerDweight\RA\RA;
 
 class ArrayMethodsTest extends TestCase
 {
     public function testPush(): void
     {
+        $ra = new RA();
+        $this->assertSame(0, $ra->length());
+        $ra->push('test1');
+        $this->assertSame(1, $ra->length());
+        $ra->push('test2', 'test3');
+        $this->assertSame(3, $ra->length());
+        $this->assertSame('test1', $ra[0]);
+        $this->assertSame('test2', $ra[1]);
+        $this->assertSame('test3', $ra[2]);
     }
 
     public function testPop(): void
     {
+        $ra = new RA(['test1', 'test2', 'test3']);
+        $this->assertSame(3, $ra->length());
+        $this->assertSame('test3', $ra->pop());
+        $this->assertSame(2, $ra->length());
+        $this->expectException(RAException::class);
+        $this->expectExceptionCode(RAException::INVALID_OFFSET);
+        $ra->offsetGet(2);
     }
 
     public function testChangeKeyCase(): void
     {
+        $ra = new RA(['a' => 'test1', 'b' => 'test2', 'c' => 'test3']);
+        $this->assertSame(['a', 'b', 'c'], $ra->keys()->toArray());
+        $ra2 = $ra->changeKeyCase(CASE_UPPER);
+        $this->assertSame(['a', 'b', 'c'], $ra->keys()->toArray());
+        $this->assertNotSame(['a', 'b', 'c'], $ra2->keys()->toArray());
+        $this->assertSame(['A', 'B', 'C'], $ra2->keys()->toArray());
     }
 
     public function testChunk(): void
     {
+        $ra = new RA(['a' => 'test1', 'b' => 'test2', 'c' => 'test3', 'd' => 'test4', 'e' => 'test5']);
+
+        $chunks = $ra->chunk(2);
+        $this->assertSame(5, $ra->length());
+        $this->assertSame(3, $chunks->length());
+        $this->assertSame(2, $chunks[0]->length());
+        $this->assertSame(2, $chunks[1]->length());
+        $this->assertSame(1, $chunks[2]->length());
+
+        $associativeChunks = $ra->chunk(2, true);
+        $this->assertSame(5, $ra->length());
+        $this->assertSame([0, 1, 2], $associativeChunks->keys()->toArray());
+        $this->assertSame(['a', 'b'], $associativeChunks[0]->keys()->toArray());
+        $this->assertSame(['c', 'd'], $associativeChunks[1]->keys()->toArray());
+        $this->assertSame(['e'], $associativeChunks[2]->keys()->toArray());
     }
 
     public function testColumn(): void
     {
+        $ra = new RA([
+            ['id' => 'abcd', 'title' => 'Test 1', 'price' => 123],
+            ['id' => 'efgh', 'title' => 'Test 2', 'price' => 0],
+            ['id' => 'ijkl', 'title' => 'Test 3', 'price' => 345]
+        ]);
+
+        $titleById = $ra->column('title', 'id');
+        $priceByTitle = $ra->column('price', 'title');
+        $this->assertSame(['abcd' => 'Test 1', 'efgh' => 'Test 2', 'ijkl' => 'Test 3'], $titleById->toArray());
+        $this->assertSame(['Test 1' => 123, 'Test 2' => 0, 'Test 3' => 345], $priceByTitle->toArray());
     }
 
     public function testCombine(): void
